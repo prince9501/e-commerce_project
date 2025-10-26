5dbb472ef4ef0fa3864475bc1734a239dc48ffb3
prince9501 <jacobprinceka02@gmail.com>
2025-10-26 10:09:00 +0530

    price update

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -34,8 +34,8 @@ const ProductDisplay = (props) => {
             
         </div>
         <div className="productdisplay-right-prices">
-            <div className="productdisplay-right-price-old">${product.old_price}</div>
-            <div className="productdisplay-right-price-new">${product.new_price}</div>
+            <div className="productdisplay-right-price-old">Rs {product.old_price}</div>
+            <div className="productdisplay-right-price-new">Rs {product.new_price}</div>
         </div>
         <div className="productdisplay-right-description">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
